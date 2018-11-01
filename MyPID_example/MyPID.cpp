35ec9c3e6db72a4065688dd2607d46ffa8a624e9
Janek <jan.wielgus12@gmail.com>
2018-11-01 14:27:05 +0100

    Implementation of the PID class interface

--- MyPID_example/MyPID.cpp
+++ MyPID_example/MyPID.cpp
@@ -5,9 +5,98 @@
 
 #include "MyPID.h"
 
-MyPID::MyPID(ParamType par);
+MyPID::MyPID(uint16_t interval, float kP=0.0, float kI=0.0, float kD=0.0, uint16_t Imax=0)
 {
-	parameters = par;
+	this->params.kP = kP;
+	this->params.kI = kI;
+	this->params.kD = kD;
+	this->params.Imax = Imax;
+	this->interval = interval;
 }
+
+
+double MyPID::updateController(double setPoint, double newValue)
+{
+	return updateController(setPoint-newValue);
+}
+
+
+double MyPID::updateController(double newError);
+{
+	//////////////////////////////////////////////////////////////////////////
+	// Not yet implemented
+	//////////////////////////////////////////////////////////////////////////
+}
+
+
+void MyPID::setParameters(float kP, float kI, float kD, uint16_t Imax)
+{
+	set_kP(kP);
+	set_kI(kI);
+	set_kD(kD);
+	set_Imax(Imax);
+}
+
+void MyPID::set_kP(float kP)
+{
+	this->params.kP = kP;
+}
+
+void MyPID::set_kI(float kI)
+{
+	this->params.kI = kI;
+}
+
+void MyPID::set_kD(float kD)
+{
+	this->params.kD = kD;
+}
+
+void MyPID:: set_Imax(uint16_t imax)
+{
+	this->params.Imax = imax;
+}
+
+float MyPID::get_kP()
+{
+	return params.kP;
+}
+
+float MyPID::get_kI()
+{
+	return params.kI;
+}
+
+float MyPID::get_kD()
+{
+	return params.kD;
+}
+
+uint16_t MyPID::get_Imax()
+{
+	return params.Imax;
+}
+
+
+void MyPID::setInterval(uint16_t interval)
+{
+	this->interval = interval;
+}
+
+uint16_t MyPID::getInterval()
+{
+	return interval;
+}
+
+
+void MyPID::resetController()
+{
+	//////////////////////////////////////////////////////////////////////////
+	// Not yet implemented
+	//////////////////////////////////////////////////////////////////////////
+}
+
+
+
 
 
--- MyPID_example/MyPID.h
+++ MyPID_example/MyPID.h
@@ -12,34 +12,43 @@
 	#include "WProgram.h"
 #endif
 
-struct ParamType
-{
-	float kP;
-	float kI;
-	float kD;
-	uint16_t Imax;
-};
-
 
 class MyPID
 {
 	public:
-		MyPID(ParamType par=0);
-		
-		ParamType getParameters();
-		void setParameters(ParamType par);
-		double updateController(double newError);
-		static double getDeltaTime() { return deltaT; }
-		static void updateDeltaTime();
+		MyPID(uint16_t, float, float, float, uint16_t); // interv, kp, ki, kd, imax
+		double updateController(double); // newError
+		double updateController(double, double); // setPoint, newValue
+		void setParameters(float, float, float, uint16_t);
+		void set_kP(float);
+		void set_kI(float);
+		void set_kD(float);
+		void set_Imax(uint16_t);
+		float get_kP();
+		float get_kI();
+		float get_kD();
+		uint16_t get_Imax();
+		void setInterval(uint16_t);
+		uint16_t getInterval();
 		void resetController();
 		
 		
 		
 	private:
-		ParamType parameters;
+	/*
 		static double deltaT; // [s]
 		double lastError;
-		double integral;
+		double integral;*/
+	
+	uint16_t interval; // time will not be calculated, just have to be run every interval (in milliseconds)
+		
+		struct
+		{
+			float kP;
+			float kI;
+			float kD;
+			uint16_t Imax;
+		} params;
 		
 };
 
